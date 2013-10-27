c1c447116d44af47aee87429f0a258d009c1c976
Alexander Olsson <noseglid@gmail.com>
2013-10-27 17:50:43 +0100

    tab -> space
    Following coding convention, use spaces rather than tabs.

--- src/Emitter.h
+++ src/Emitter.h
@@ -7,119 +7,119 @@
 #include <string>
 
 namespace de {
-	struct __nodata {};
+  struct __nodata {};
 
-	struct key {
-		int id;
-		std::string event;
+  struct key {
+    int id;
+    std::string event;
 
-		key(int id, std::string event) : id(id), event(event) {}
-		bool operator<(const key& rhs) const { return event < rhs.event; }
-	};
+    key(int id, std::string event) : id(id), event(event) {}
+    bool operator<(const key& rhs) const { return event < rhs.event; }
+  };
 
-	template <class T = struct __nodata>
-	class Emitter
-	{
-		typedef std::function< void(T) > event_cb;
-		typedef std::multimap<key, event_cb> mm_t;
+  template <class T = struct __nodata>
+  class Emitter
+  {
+    typedef std::function< void(T) > event_cb;
+    typedef std::multimap<key, event_cb> mm_t;
 
-		mm_t consumers;
+    mm_t consumers;
 
-		int cur_id;
-		std::vector<int> erased;
+    int cur_id;
+    std::vector<int> erased;
 
-	protected:
-		Emitter() : cur_id(0)
-		{
-		}
+  protected:
+    Emitter() : cur_id(0)
+    {
+    }
 
-	public:
-		typedef typename mm_t::iterator id_t;
+  public:
+    typedef typename mm_t::iterator id_t;
 
-		id_t on(std::string event, event_cb cb)
-		{
-			return consumers.insert(std::make_pair(key(cur_id++, event), cb));
-		}
+    id_t on(std::string event, event_cb cb)
+    {
+      return consumers.insert(std::make_pair(key(cur_id++, event), cb));
+    }
 
-		void off(id_t id)
-		{
-			erased.push_back(id->first.id);
-			consumers.erase(id);
-		}
+    void off(id_t id)
+    {
+      erased.push_back(id->first.id);
+      consumers.erase(id);
+    }
 
-		void disable(std::string event)
-		{
-			consumers.erase(key(-1, event));
-		}
+    void disable(std::string event)
+    {
+      consumers.erase(key(-1, event));
+    }
 
-		void emit(std::string event, T data)
-		{
-			mm_t tmp = consumers;
-			auto range = tmp.equal_range(key(-1, event));
-			auto it = range.first;
-			while (it != range.second) {
-				auto current = it++;
+    void emit(std::string event, T data)
+    {
+      mm_t tmp = consumers;
+      auto range = tmp.equal_range(key(-1, event));
+      auto it = range.first;
+      while (it != range.second) {
+        auto current = it++;
 
-				auto found = std::find(erased.begin(), erased.end(), current->first.id);
-				if (erased.end() != found) continue;
+        auto found = std::find(erased.begin(), erased.end(), current->first.id);
+        if (erased.end() != found) continue;
 
-				(current->second)(data);
-			}
-			erased.clear();
-		}
-	};
+        (current->second)(data);
+      }
+      erased.clear();
+    }
+  };
 
-	template <>
-	class Emitter <struct __nodata>
-	{
-		typedef std::function< void() > event_cb;
-		typedef std::multimap<key, event_cb> mm_t;
+  template <>
+  class Emitter <struct __nodata>
+  {
+    typedef std::function< void() > event_cb;
+    typedef std::multimap<key, event_cb> mm_t;
 
-		mm_t consumers;
+    mm_t consumers;
 
-		int cur_id;
-		std::vector<int> erased;
+    int cur_id;
+    std::vector<int> erased;
 
-	protected:
-		Emitter() : cur_id(0)
-		{
-		}
+  protected:
+    Emitter() : cur_id(0)
+    {
+    }
 
-	public:
-		typedef typename mm_t::iterator id_t;
+  public:
+    typedef typename mm_t::iterator id_t;
 
-		id_t on(std::string event, event_cb cb)
-		{
-			return consumers.insert(std::make_pair(key(cur_id++, event), cb));
-		}
+    id_t on(std::string event, event_cb cb)
+    {
+      return consumers.insert(std::make_pair(key(cur_id++, event), cb));
+    }
 
-		void off(id_t id)
-		{
-			erased.push_back(id->first.id);
-			consumers.erase(id);
-		}
+    void off(id_t id)
+    {
+      erased.push_back(id->first.id);
+      consumers.erase(id);
+    }
 
-		void disable(std::string event)
-		{
-			consumers.erase(key(-1, event));
-		}
+    void disable(std::string event)
+    {
+      consumers.erase(key(-1, event));
+    }
 
-		void emit(std::string event)
-		{
-			mm_t tmp = consumers;
-			auto range = tmp.equal_range(key(-1, event));
-			auto it = range.first;
-			while (it != range.second) {
-				auto current = it++;
+    void emit(std::string event)
+    {
+      mm_t tmp = consumers;
+      auto range = tmp.equal_range(key(-1, event));
+      auto it = range.first;
+      while (it != range.second) {
+        auto current = it++;
 
-				auto found = std::find(erased.begin(), erased.end(), current->first.id);
-				if (erased.end() != found) continue;
+        auto found = std::find(erased.begin(), erased.end(), current->first.id);
+        if (erased.end() != found) continue;
 
-				(current->second)();
-			}
-			erased.clear();
-		}
-	};
+        (current->second)();
+      }
+      erased.clear();
+    }
+  };
 };
 
 
--- tests/basic.cpp
+++ tests/basic.cpp
@@ -6,210 +6,210 @@
 class em : public de::Emitter<>
 {
 public:
-	void emit_fn()
-	{
-		emit("signal");
-	}
+  void emit_fn()
+  {
+    emit("signal");
+  }
 };
 
 BOOST_AUTO_TEST_CASE( on_lambda )
 {
-	em e;
-	bool called = false;
-	auto fn = [&called](void) { called = true; };
+  em e;
+  bool called = false;
+  auto fn = [&called](void) { called = true; };
 
-	e.on("signal", fn);
-	e.emit_fn();
+  e.on("signal", fn);
+  e.emit_fn();
 
-	BOOST_CHECK(called);
+  BOOST_CHECK(called);
 }
 
 void fn(bool& called)
 {
-	called = true;
+  called = true;
 }
 
 BOOST_AUTO_TEST_CASE( on_function )
 {
-	em e;
-	bool called = false;
-	e.on("signal", std::bind(fn, std::ref(called)));
+  em e;
+  bool called = false;
+  e.on("signal", std::bind(fn, std::ref(called)));
 
-	e.emit_fn();
+  e.emit_fn();
 
-	BOOST_CHECK(called);
+  BOOST_CHECK(called);
 }
 
 class clfn
 {
 public:
-	void fn(bool& called)
-	{
-		called = true;
-	}
+  void fn(bool& called)
+  {
+    called = true;
+  }
 };
 
 BOOST_AUTO_TEST_CASE( on_method )
 {
-	em e;
-	clfn c;
-	bool called = false;
-	e.on("signal", std::bind(&clfn::fn, &c, std::ref(called)));
+  em e;
+  clfn c;
+  bool called = false;
+  e.on("signal", std::bind(&clfn::fn, &c, std::ref(called)));
 
-	e.emit_fn();
+  e.emit_fn();
 
-	BOOST_CHECK(called);
+  BOOST_CHECK(called);
 }
 
 BOOST_AUTO_TEST_CASE( on_off )
 {
-	em e;
-	bool called = false;
-	auto cb = [&called]() { called = true; };
+  em e;
+  bool called = false;
+  auto cb = [&called]() { called = true; };
 
-	de::Emitter<>::id_t cbid = e.on("signal", cb);
-	e.emit_fn();
-	BOOST_CHECK(called);
+  de::Emitter<>::id_t cbid = e.on("signal", cb);
+  e.emit_fn();
+  BOOST_CHECK(called);
 
-	called = false;
-	e.off(cbid);
-	e.emit_fn();
-	BOOST_CHECK_EQUAL(called, false);
+  called = false;
+  e.off(cbid);
+  e.emit_fn();
+  BOOST_CHECK_EQUAL(called, false);
 }
 
 BOOST_AUTO_TEST_CASE( disable )
 {
-	em e;
-	bool called1 = false, called2 = false;
-	auto cb1 = [&called1]() { called1 = true; };
-	auto cb2 = [&called2]() { called2 = true; };
+  em e;
+  bool called1 = false, called2 = false;
+  auto cb1 = [&called1]() { called1 = true; };
+  auto cb2 = [&called2]() { called2 = true; };
 
-	e.on("signal", cb1);
-	e.on("signal", cb2);
+  e.on("signal", cb1);
+  e.on("signal", cb2);
 
-	e.emit_fn();
+  e.emit_fn();
 
-	BOOST_CHECK(called1);
-	BOOST_CHECK(called2);
+  BOOST_CHECK(called1);
+  BOOST_CHECK(called2);
 
-	called1 = called2 = false;
+  called1 = called2 = false;
 
-	e.disable("signal");
-	e.emit_fn();
+  e.disable("signal");
+  e.emit_fn();
 
-	BOOST_CHECK_EQUAL(called1, false);
-	BOOST_CHECK_EQUAL(called2, false);
+  BOOST_CHECK_EQUAL(called1, false);
+  BOOST_CHECK_EQUAL(called2, false);
 }
 
 BOOST_AUTO_TEST_CASE( emit_delete )
 {
-	em *e = new em();
-	e->on("signal", [e]() { delete e; });
+  em *e = new em();
+  e->on("signal", [e]() { delete e; });
 
-	e->emit_fn();
+  e->emit_fn();
 
-	BOOST_CHECK(true);
+  BOOST_CHECK(true);
 }
 
 BOOST_AUTO_TEST_CASE( alter_1st_consumers_in_cb )
 {
-	em e;
-	de::Emitter<>::id_t idcb1, idcb2;
+  em e;
+  de::Emitter<>::id_t idcb1, idcb2;
 
-	auto cb1 = [&idcb1, &e]() {
-		e.off(idcb1);
-	};
+  auto cb1 = [&idcb1, &e]() {
+    e.off(idcb1);
+  };
 
-	bool called = false;
-	auto cb2 = [&called]() {
-		called = true;
-	};
+  bool called = false;
+  auto cb2 = [&called]() {
+    called = true;
+  };
 
-	idcb1 = e.on("signal", cb1);
-	idcb2 = e.on("signal", cb2);
+  idcb1 = e.on("signal", cb1);
+  idcb2 = e.on("signal", cb2);
 
-	e.emit_fn();
+  e.emit_fn();
 
-	BOOST_CHECK(called);
+  BOOST_CHECK(called);
 }
 
 BOOST_AUTO_TEST_CASE( alter_2nd_consumers_in_cb )
 {
-	em e;
-	de::Emitter<>::id_t idcb1, idcb2;
+  em e;
+  de::Emitter<>::id_t idcb1, idcb2;
 
-	auto cb1 = [&idcb1, &idcb2, &e]() {
-		e.off(idcb1);
-		e.off(idcb2);
-	};
+  auto cb1 = [&idcb1, &idcb2, &e]() {
+    e.off(idcb1);
+    e.off(idcb2);
+  };
 
-	bool called = false;
-	auto cb2 = [&called]() {
-		called = true;
-	};
+  bool called = false;
+  auto cb2 = [&called]() {
+    called = true;
+  };
 
-	idcb1 = e.on("signal", cb1);
-	idcb2 = e.on("signal", cb2);
+  idcb1 = e.on("signal", cb1);
+  idcb2 = e.on("signal", cb2);
 
-	e.emit_fn();
+  e.emit_fn();
 
-	/**
-	 * No crash, excellent, and 2nd cb should not be called
-	 * as it should've been removed during cb1.
-	 */
-	BOOST_CHECK_EQUAL(called, false);
+  /**
+   * No crash, excellent, and 2nd cb should not be called
+   * as it should've been removed during cb1.
+   */
+  BOOST_CHECK_EQUAL(called, false);
 }
 
 BOOST_AUTO_TEST_CASE ( delete_upcomming_cb )
 {
-	em e;
-	de::Emitter<>::id_t idcb1, idcb2;
+  em e;
+  de::Emitter<>::id_t idcb1, idcb2;
 
-	auto cb1 = [&idcb1, &idcb2, &e]() {
-		e.off(idcb1);
-		e.off(idcb2);
-	};
+  auto cb1 = [&idcb1, &idcb2, &e]() {
+    e.off(idcb1);
+    e.off(idcb2);
+  };
 
-	bool called = false;
-	auto cb2 = [&called]() {
-		called = true;
-	};
+  bool called = false;
+  auto cb2 = [&called]() {
+    called = true;
+  };
 
-	idcb1 = e.on("signal", cb1);
-	idcb2 = e.on("signal", cb2);
+  idcb1 = e.on("signal", cb1);
+  idcb2 = e.on("signal", cb2);
 
-	e.emit_fn();
+  e.emit_fn();
 }
 
 class recv
 {
-	de::Emitter<>::id_t id;
+  de::Emitter<>::id_t id;
 public:
-	void fn(de::Emitter<>& e, bool& called)
-	{
-		e.off(this->id);
-		called = true;
-	}
+  void fn(de::Emitter<>& e, bool& called)
+  {
+    e.off(this->id);
+    called = true;
+  }
 
-	void set(de::Emitter<>::id_t id)
-	{
-		this->id = id;
-	}
+  void set(de::Emitter<>::id_t id)
+  {
+    this->id = id;
+  }
 };
 
 BOOST_AUTO_TEST_CASE( off_upcomming_consumer )
 {
-	em e;
-	recv re;
-	bool called = false;
-	bool not_called = false;
+  em e;
+  recv re;
+  bool called = false;
+  bool not_called = false;
 
-	e.on("signal", std::bind(&recv::fn, &re, std::ref(e), std::ref(called)));
-	de::Emitter<>::id_t idcb = e.on("signal", [&not_called]() { not_called = true; });
-	re.set(idcb);
+  e.on("signal", std::bind(&recv::fn, &re, std::ref(e), std::ref(called)));
+  de::Emitter<>::id_t idcb = e.on("signal", [&not_called]() { not_called = true; });
+  re.set(idcb);
 
-	e.emit_fn();
+  e.emit_fn();
 
-	BOOST_CHECK_EQUAL(called, true);
-	BOOST_CHECK_EQUAL(not_called, false);
+  BOOST_CHECK_EQUAL(called, true);
+  BOOST_CHECK_EQUAL(not_called, false);
 }
